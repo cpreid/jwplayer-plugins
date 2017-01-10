5decf36a03f263e0b12f9147ec60cec086140709
Cooper Reid <cooper@coopers-mbp.office.longtailvideo.com>
2017-01-10 16:14:40 -0500

    add overlay plugin

---
+++ jwoverlay/jwoverlay.js
@@ -0,0 +1,70 @@
+(function() {
+
+    var Overlay = function(player) {
+        var overlay = document.createElement('div');     
+
+        // any element with class 'jwoverlay-close' will close the overlay
+        overlay.addEventListener('click', function(evt) {
+            var clicked = evt.target || evt.toElement;
+            if(clicked.className.split(/\s+/).indexOf('jwoverlay-close') > -1) {
+                api.hide();
+            }
+            else if(clicked.className.split(/\s+/).indexOf('jwoverlay-play') > -1) {
+                api.hide();
+                try {
+                    player.play();
+                } catch(err) { }                
+            }
+        });      
+
+        // api methods are chainable
+        var api = {            
+            show: function() {
+                overlay.style.display = 'block';
+                return api;
+            },
+            css: function(cssProperties) {
+                for(p in cssProperties) {
+                    overlay.style[p] = cssProperties[p];
+                }
+                return api;
+            },
+            resetCss: function() {
+                overlay.style.cssText += 'position:absolute;left:0;right:0;padding:10px;color:#FFF;';
+                overlay.style.cssText += 'height:100%;width:100%;background:rgba(0,0,0,.8);z-index:99';
+                return api;
+            },
+            hide: function() {
+                overlay.style.display = 'none';
+                return api;
+            },
+            html: function(html) {
+                overlay.innerHTML = html || '';
+                return api;
+            },
+            init: function() {
+                overlay.innerHTML     =  '';        
+                overlay.className     = 'jwoverlay';
+                overlay.style.display = 'none';      
+                return api;          
+            },
+            elt: function() {
+                return overlay;
+            }
+        };    
+
+        // initiallize div, reset style
+        api.init().resetCss();
+        return api;
+    }
+
+    window.jwoverlay = function(_player) {
+        var overlay = Overlay(_player);        
+        // attach overlay object to player instance
+        _player.jwoverlay = overlay;
+        _player.on('ready', function() {
+            _player.getContainer().appendChild(overlay.elt());
+        });
+    }    
+
+})();
